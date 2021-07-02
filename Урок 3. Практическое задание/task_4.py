90a29e28525911c8f83c0e344dddfd0f9363c1d8
solovyova <shatdar@yandex.ru>
2021-07-02 20:56:39 +0600

    dz3

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -11,3 +11,25 @@
 
 Задание творческое. Здесь нет жестких требований к выполнению.
 """
+from uuid import uuid4
+from hashlib import sha256
+
+
+def cache_web(url, cache):
+    if cache.get(url) is None:
+        salt = uuid4().hex
+        cache[url] = sha256(salt.encode() + url.encode()).hexdigest()
+
+
+cache_1 = dict()
+# 2 одинаковых url
+url_1 = 'https://edu.omgpu.ru/login/index.php'
+url_2 = 'https://edu.omgpu.ru/login/index.php'
+# url отличается
+url_3 = 'https://gb.ru/'
+# вызов функции с url
+res_1 = cache_web(url_1, cache_1)
+res_2 = cache_web(url_2, cache_1)
+res_3 = cache_web(url_3, cache_1)
+# просмотр заполненного словаря
+print(cache_1)
