f1fb5c1b6fa6db49f0f7e83443ec179c36c88340
solovyova <shatdar@yandex.ru>
2021-07-16 23:03:21 +0600

    dz7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -17,3 +17,144 @@
 Сделайте выводы!!!
 Опишите в чем была ваша доработка и помогла ли вам доработка??
 """
+"""Сортировка пузырьком"""
+
+import timeit
+import random
+
+
+################## Оригинальная функция из примера в уроке #####################
+
+def bubble_sort(lst_obj):
+    n = 1
+    while n < len(lst_obj):
+        for i in range(len(lst_obj) - n):
+            if lst_obj[i] > lst_obj[i + 1]:
+                lst_obj[i], lst_obj[i + 1] = lst_obj[i + 1], lst_obj[i]
+        n += 1
+    return lst_obj
+
+
+# замеры 10
+# orig_list = [random.randint(-100, 100) for _ in range(10)]
+# print(
+#     timeit.timeit(
+#         "bubble_sort(orig_list[:])",
+#         globals=globals(),
+#         number=1000))
+#
+# orig_list = [random.randint(-100, 100) for _ in range(100)]
+#
+# # замеры 100
+# print(
+#     timeit.timeit(
+#         "bubble_sort(orig_list[:])",
+#         globals=globals(),
+#         number=1000))
+#
+# orig_list = [random.randint(-100, 100) for _ in range(1000)]
+#
+# # замеры 1000
+# print(
+#     timeit.timeit(
+#         "bubble_sort(orig_list[:])",
+#         globals=globals(),
+#         number=1000))
+
+# 0.06876350000000003
+# 1.7818022999999998
+# 166.9776324
+############################ пузырек по убыванию ###############################
+def bubble_sort_waning(lst_obj):
+    n = 1
+    while n < len(lst_obj):
+        for i in range(len(lst_obj) - n):
+            if lst_obj[i] < lst_obj[i + 1]:
+                lst_obj[i], lst_obj[i + 1] = lst_obj[i + 1], lst_obj[i]
+        n += 1
+    return lst_obj
+
+
+# замеры 10
+# orig_list = [random.randint(-100, 100) for _ in range(10)]
+# print(
+#     timeit.timeit(
+#         "bubble_sort_waning(orig_list[:])",
+#         globals=globals(),
+#         number=1000))
+#
+# orig_list = [random.randint(-100, 100) for _ in range(100)]
+#
+# # замеры 100
+# print(
+#     timeit.timeit(
+#         "bubble_sort_waning(orig_list[:])",
+#         globals=globals(),
+#         number=1000))
+#
+# orig_list = [random.randint(-100, 100) for _ in range(1000)]
+#
+# # замеры 1000
+# print(
+#     timeit.timeit(
+#         "bubble_sort_waning(orig_list[:])",
+#         globals=globals(),
+#         number=1000))
+# 0.02145549999999999
+# 2.9664789
+# 136.9452684
+
+################# пузырек улучшеный № 1 ###############################
+# пузырек по убыванию и при сортированнном списке выходит из цикла,
+# что дает потенциальное увеличение скорости
+def bubble_sort_waning_update(lst_obj):
+    n = 1
+    change_count = 0
+    while n < len(lst_obj):
+        if n == 2 and change_count == 0:
+            print(1)
+            break
+        for i in range(len(lst_obj) - n):
+            if lst_obj[i] < lst_obj[i + 1]:
+                lst_obj[i], lst_obj[i + 1] = lst_obj[i + 1], lst_obj[i]
+                change_count += 1
+
+        n += 1
+    return lst_obj
+
+
+# замеры 10
+
+orig_list = [random.randint(-100, 100) for _ in range(10)]
+print(
+    timeit.timeit(
+        "bubble_sort_waning_update(orig_list[:])",
+        globals=globals(),
+        number=1000))
+
+orig_list = [random.randint(-100, 100) for _ in range(100)]
+
+# замеры 100
+print(
+    timeit.timeit(
+        "bubble_sort_waning_update(orig_list[:])",
+        globals=globals(),
+        number=1000))
+
+orig_list = [random.randint(-100, 100) for _ in range(1000)]
+
+# замеры 1000
+print(
+    timeit.timeit(
+        "bubble_sort_waning_update(orig_list[:])",
+        globals=globals(),
+        number=1000))
+# 0.019133700000000003
+# 1.9433184
+# 239.9567904
+
+# вывод: выход из цикла при попаданиии в функцию отсортированного списка
+# дает только потенциальное увеличение скорости работы функции, то есть функция
+# отработаем быстрее только если массив будет отсортирован, на большом массиве
+# происходит замедление функции bubble_sort_waning_update, в сравнении с функцией bubble_sort
+
--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -13,3 +13,84 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+import random
+from timeit import timeit
+
+
+def merge(left_list, right_list):
+    sorted_list = []
+    left_list_index = right_list_index = 0
+
+    # Длина списков часто используется, поэтому создадим переменные для удобства
+    left_list_length, right_list_length = len(left_list), len(right_list)
+
+    for _ in range(left_list_length + right_list_length):
+        if left_list_index < left_list_length and right_list_index < right_list_length:
+            # Сравниваем первые элементы в начале каждого списка
+            # Если первый элемент левого подсписка меньше, добавляем его
+            # в отсортированный массив
+            if left_list[left_list_index] <= right_list[right_list_index]:
+                sorted_list.append(left_list[left_list_index])
+                left_list_index += 1
+            # Если первый элемент правого подсписка меньше, добавляем его
+            # в отсортированный массив
+            else:
+                sorted_list.append(right_list[right_list_index])
+                right_list_index += 1
+
+        # Если достигнут конец левого списка, элементы правого списка
+        # добавляем в конец результирующего списка
+        elif left_list_index == left_list_length:
+            sorted_list.append(right_list[right_list_index])
+            right_list_index += 1
+        # Если достигнут конец правого списка, элементы левого списка
+        # добавляем в отсортированный массив
+        elif right_list_index == right_list_length:
+            sorted_list.append(left_list[left_list_index])
+            left_list_index += 1
+
+    return sorted_list
+
+
+def merge_sort(nums):
+    # Возвращаем список, если он состоит из одного элемента
+    if len(nums) <= 1:
+        return nums
+
+    # Для того чтобы найти середину списка, используем деление без остатка
+    # Индексы должны быть integer
+    mid = len(nums) // 2
+
+    # Сортируем и объединяем подсписки
+    left_list = merge_sort(nums[:mid])
+    right_list = merge_sort(nums[mid:])
+
+    # Объединяем отсортированные списки в результирующий
+    return merge(left_list, right_list)
+
+
+
+nums_el = int(input('Введите число элементов: '))
+lst_start = [random.triangular(0, 50) for _ in range(nums_el)]
+print(f'Исходный: {lst_start}')
+lst_sort = merge_sort(lst_start)
+print(f'Отсортированный: {lst_sort}')
+
+# замеры на списке из 10 элементов
+lst_start = [random.triangular(0, 50) for _ in range(10)]
+print(f"Время выполнения на списке из 10 элементов - "
+      f"{timeit('merge_sort(lst_start)', globals=globals(), number=1000)} сек")
+
+# замеры на списке из 100 элементов
+lst_start = [random.triangular(0, 50) for _ in range(100)]
+print(f"Время выполнения на списке из 100 элементов - "
+      f"{timeit('merge_sort(lst_start)', globals=globals(), number=1000)} сек")
+# замеры на списке из 1000 элементов
+lst_start = [random.triangular(0, 50) for _ in range(1000)]
+print(f"Время выполнения на списке из 1000 элементов - "
+      f"{timeit('merge_sort(lst_start)', globals=globals(), number=1000)} сек")
+# Время выполнения на списке из 10 элементов - 0.03362580000000026 сек
+# Время выполнения на списке из 100 элементов - 0.48340150000000026 сек
+# Время выполнения на списке из 1000 элементов - 6.5670112 сек
+# Вывод: сортировка слиянием достаточно эффективна, так как рост скорости работы
+# функции увеличивается не сильно на массиве большой длины
